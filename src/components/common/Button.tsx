55f2ed043bec643eac46513de8f2172b22b7e208
ajay <ajayjarwal44@gmail.com>
2023-12-30 13:35:02 +0530

    add a reusable Button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface ButtonProps {
+  onClick?: () => void;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, className, children }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-blue-600 hover:bg-blue-900 text-white py-2 px-4 rounded ${
+        className || ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
